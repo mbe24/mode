734dbbfaf8bdfebdc63de9616db8fc698c983b71
Mikael Beyene <dev@beyene.org>
2014-09-26 00:36:54 +0200

    Minor fixes.Fix warnings.Reconfigure tests.

--- service.test/src/test/java/org/beyene/mode/service/test/ServiceTestCase.java
+++ service.test/src/test/java/org/beyene/mode/service/test/ServiceTestCase.java
@@ -9,33 +9,46 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.cm.ConfigurationAdmin;
 
 @RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
 public class ServiceTestCase {
 
 	@Inject
     private BundleContext bundleContext;
  
-    @Inject
-    private ConfigurationAdmin configurationAdmin;
+//    @Inject
+//    private ConfigurationAdmin configurationAdmin;
 
+    @Inject
     private Service service;
     
 	@Configuration
 	public Option[] config() {
-		
 		return CoreOptions.options(
-				CoreOptions.mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.6.2"),
+				CoreOptions.mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.8.2"),
+				CoreOptions.mavenBundle("org.eclipse.osgi", "org.eclipse.osgi.services", "3.2.100.v20100503"),
+				CoreOptions.bundle("reference:file:../service/target/service-0.0.1-SNAPSHOT.jar"),
+				CoreOptions.bundle("reference:file:../service.impl/target/service.impl-0.0.1-SNAPSHOT.jar"),
 				CoreOptions.junitBundles());
 	}
-
+	
 	@Test
-	public void getService() {
-		Assert.assertEquals("This service implementation should add the numbers", 25, service.add(15, 10));
+	public void testInjections() {
+		Assert.assertNotNull(bundleContext);
+//		Assert.assertNotNull(configurationAdmin);
+		Assert.assertNotNull(service);
 	}
+	
+//	@Test
+//	public void getService() {
+//		Assert.assertEquals("This service implementation should add the numbers", 25, service.add(15, 10));
+//	}
 	
 	public void setService(Service service) {
 		this.service = service;
