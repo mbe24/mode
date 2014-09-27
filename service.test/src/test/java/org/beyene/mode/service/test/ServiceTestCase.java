692fe0ef4194113dcd3a1bfe01d42fe5ae5cd871
Mikael Beyene <dev@beyene.org>
2014-09-27 15:14:58 +0200

    Remove unnecessary osgi.bnd and component.xml from service.test. Delete webconsole dependencies.

--- service.test/src/test/java/org/beyene/mode/service/test/ServiceTestCase.java
+++ service.test/src/test/java/org/beyene/mode/service/test/ServiceTestCase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Mikael Beyene
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package org.beyene.mode.service.test;
 
 import java.util.Calendar;
@@ -6,7 +22,6 @@ import javax.inject.Inject;
 
 import org.beyene.mode.service.Service;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.CoreOptions;
@@ -23,8 +38,8 @@ public class ServiceTestCase {
 	@Configuration
 	public Option[] config() {
 		return CoreOptions.options(
+				/* needed for ds annotations */
 				CoreOptions.mavenBundle("org.apache.felix", "org.apache.felix.scr", "1.8.2"),
-				CoreOptions.mavenBundle("org.eclipse.osgi", "org.eclipse.osgi.services", "3.2.100.v20100503"),
 				CoreOptions.bundle("reference:file:../service/target/service-0.0.1-SNAPSHOT.jar"),
 				CoreOptions.bundle("reference:file:../service.impl/target/service.impl-0.0.1-SNAPSHOT.jar"),
 				CoreOptions.junitBundles());
@@ -48,11 +63,5 @@ public class ServiceTestCase {
 		Calendar b = Calendar.getInstance();
 		
 		Assert.assertEquals(a.get(Calendar.YEAR), b.get(Calendar.YEAR));
-	}
-	
-	// needed by eclipse, current setup does not support dc annotations
-	@Ignore
-	public void setService(Service service) {
-		this.service = service;
 	}
 }
