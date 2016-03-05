d3d679dbc342219d3de79f71a929f60e8ab64a39
Mikael Beyene <mikael@beyene.org>
2016-03-05 02:11:29 +0100

    Update dependency versions Fix #6

--- service.test/src/test/java/org/beyene/mode/service/test/ServiceTestCase.java
+++ service.test/src/test/java/org/beyene/mode/service/test/ServiceTestCase.java
@@ -26,10 +26,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.junit.PaxExam;;
 
-@RunWith(JUnit4TestRunner.class)
+@RunWith(PaxExam.class)
 public class ServiceTestCase {
 
     @Inject
